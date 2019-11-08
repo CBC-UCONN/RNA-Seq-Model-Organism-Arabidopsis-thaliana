3eedcd96a437e6e4ad58bc80188eadadc5cf23db
Your Name <you@example.com>
2019-11-08 13:47:16 -0500

    script add

--- raw_data/data_dump.sh
+++ raw_data/data_dump.sh
@@ -10,29 +10,31 @@
 #SBATCH -e data_dump_%j.err
 #SBATCH --partition=general
 #SBATCH --qos=general
+
+
 export TMPDIR=/home/CAM/$USER/tmp/
 
 module load sratoolkit
 
-#fastq-dump --split-files SRR8428909 
-#mv SRR8428909_1.fastq wt_Rep1_R1.fastq
-#mv SRR8428909_2.fastq wt_Rep1_R2.fastq
+fastq-dump --split-files SRR8428909 
+mv SRR8428909_1.fastq wt_Rep1_R1.fastq
+mv SRR8428909_2.fastq wt_Rep1_R2.fastq
 
-#fastq-dump --split-files SRR8428908
-#mv SRR8428908_1.fastq wt_Rep2_R1.fastq
-#mv SRR8428908_2.fastq wt_Rep2_R2.fastq
+fastq-dump --split-files SRR8428908
+mv SRR8428908_1.fastq wt_Rep2_R1.fastq
+mv SRR8428908_2.fastq wt_Rep2_R2.fastq
  
-#fastq-dump --split-files SRR8428907 
-#mv SRR8428907_1.fastq wt_Rep3_R1.fastq
-#mv SRR8428907_2.fastq wt_Rep3_R2.fastq
+fastq-dump --split-files SRR8428907 
+mv SRR8428907_1.fastq wt_Rep3_R1.fastq
+mv SRR8428907_2.fastq wt_Rep3_R2.fastq
 
-#fastq-dump --split-files SRR8428906 
-#mv SRR8428906_1.fastq mutant_Rep1_R1.fastq
-#mv SRR8428906_2.fastq mutant_Rep1_R2.fastq
+fastq-dump --split-files SRR8428906 
+mv SRR8428906_1.fastq mutant_Rep1_R1.fastq
+mv SRR8428906_2.fastq mutant_Rep1_R2.fastq
 
-#fastq-dump --split-files SRR8428905 
-#mv SRR8428905_1.fastq mutant_Rep2_R1.fastq
-#mv SRR8428905_2.fastq mutant_Rep2_R2.fastq
+fastq-dump --split-files SRR8428905 
+mv SRR8428905_1.fastq mutant_Rep2_R1.fastq
+mv SRR8428905_2.fastq mutant_Rep2_R2.fastq
 
 fastq-dump --split-files SRR8428904 
 mv SRR8428904_1.fastq mutant_Rep3_R1.fastq
